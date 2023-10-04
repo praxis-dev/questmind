5a8a60a8a72f29d83de28bf4d2c17d8c7f4c7b0f
praxis-dev <chesnokovprm@gmail.com>
2023-10-03 19:41:17 -0600

    feat: new structure

---
+++ app/ingest.py
@@ -0,0 +1,43 @@
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+
+DATA_PATH = "../data/texts"
+DB_FAISS_PATH = "../vectorstores/db_faiss"
+QUESTIONS_PATH = os.path.join("../", 'data', 'questions', 'questions.txt')
+
+
+def ingest_questions():
+
+    questions = []
+
+    with open(QUESTIONS_PATH, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                questions.append(line)
+
+    return questions
+
+
+def create_vector_db():
+    loader = DirectoryLoader(DATA_PATH, glob="*.txt", loader_cls=TextLoader)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500, chunk_overlap=25)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"}
+    )
+
+    db = FAISS.from_documents(texts, embeddings)
+    db.save_local(DB_FAISS_PATH)
+
+
+if __name__ == "__main__":
+    ingest_questions()
+    create_vector_db()
