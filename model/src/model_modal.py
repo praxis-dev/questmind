bc1cfe0c418675a410e23fb03dede7d27c47e120
praxis-dev <chesnokovprm@gmail.com>
2023-10-20 13:55:18 -0600

    feat: refactoring progress

--- model/src/model_modal.py
+++ model/src/model_modal.py
@@ -11,7 +11,6 @@ image = Image.debian_slim().pip_install(
     "sentence-transformers",
     "faiss-cpu",
     "langchain",
-    "ctransformers[cuda]",
     "pydantic",
     "accelerate",
     "transformers",
@@ -52,7 +51,7 @@ def detect_device():
 def ingest_questions():
     print("Starting the ingestion of questions.")
     import torch
-    from sentence_transformers import SentenceTransformer, util
+    from sentence_transformers import SentenceTransformer
 
     questions = []
 
@@ -136,22 +135,22 @@ class RequestModel(BaseModel):
 @stub.function(image=image, gpu="T4", network_file_systems={DB_FAISS_PATH: volume}, allow_cross_region_volumes=True)
 @web_endpoint(method="POST")
 def get_response(request: RequestModel) -> str:
-    print("Initializing the QA bot.")
-
-    query = request.query 
-    
+      
     import torch
     import transformers
     from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-    from langchain import HuggingFacePipeline, LLMChain, PromptTemplate
+    from langchain import HuggingFacePipeline
     from langchain.embeddings import HuggingFaceEmbeddings
     from langchain.vectorstores import FAISS
     from langchain.chains import RetrievalQA
 
     import os
+    print("Initializing the QA bot.")
 
-    import json
-    import textwrap
+    query = request.query 
+    
+    if not is_philosophy_related.remote(query):
+        return ("This is not my area of expertise.")
     
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={"device": detect_device.remote()})
@@ -159,7 +158,6 @@ def get_response(request: RequestModel) -> str:
     created_files = os.listdir(DB_FAISS_PATH)
     for file in created_files:
         print(file)
-    
     
     db = FAISS.load_local(DB_FAISS_PATH, embeddings)
     
@@ -186,84 +184,16 @@ def get_response(request: RequestModel) -> str:
                 eos_token_id=tokenizer.eos_token_id
                 )
     
-    B_INST, E_INST = "[INST]", "[/INST]"
-    B_SYS, E_SYS = "<>\n", "\n<>\n\n"
-    DEFAULT_SYSTEM_PROMPT = """\
-    You are an advanced Life guru and mental health expert that excels at giving advice.
-    Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
-    If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.Just say you don't know and you are sorry!"""
-
-    def get_prompt(instruction, new_system_prompt=DEFAULT_SYSTEM_PROMPT, citation=None):
-        SYSTEM_PROMPT = B_SYS + new_system_prompt + E_SYS
-        prompt_template =  B_INST + SYSTEM_PROMPT + instruction + E_INST
-
-        if citation:
-            prompt_template += f"\n\nCitation: {citation}"  # Insert citation here
-
-        return prompt_template
-
-    def cut_off_text(text, prompt):
-        cutoff_phrase = prompt
-        index = text.find(cutoff_phrase)
-        if index != -1:
-            return text[:index]
-        else:
-            return text
-
-    def remove_substring(string, substring):
-        return string.replace(substring, "")
-
-    def generate(text, citation=None):
-        prompt = get_prompt(text, citation=citation)
-        inputs = tokenizer(prompt, return_tensors="pt")
-        with torch.no_grad():
-            outputs = model.generate(**inputs,
-                                    max_length=512,
-                                    eos_token_id=tokenizer.eos_token_id,
-                                    pad_token_id=tokenizer.eos_token_id,
-                                    )
-            final_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
-            final_outputs = cut_off_text(final_outputs, '')
-            final_outputs = remove_substring(final_outputs, prompt)
-
-        return final_outputs
-
-    def parse_text(text):
-        wrapped_text = textwrap.fill(text, width=100)
-        print(wrapped_text + '\n\n')
-        
-        
+   
     llm = HuggingFacePipeline(pipeline = pipe, model_kwargs = {'temperature':0.7,'max_length': 256, 'top_k' :50})
 
 
-    system_prompt = "You are an advanced Life guru and mental health expert that excels at giving advice. "
-    instruction = "Convert the following input text from a stupid human to a well-reasoned and step-by-step throughout advice:\n\n {text}"
-    template = get_prompt(instruction, system_prompt)
-    print(template)
-    
-    prompt = PromptTemplate(template=template, input_variables=["text"])
-
-    llm_chain = LLMChain(prompt=prompt, llm=llm, verbose = False)
-
-
-
-        
-    
-
-    
-
-
-
-    
     qa_chain = RetrievalQA.from_chain_type(llm=llm,
                                            chain_type='stuff',
                                            retriever=db.as_retriever(
                                                search_kwargs={'k': 2}),
                                            return_source_documents=False,
                                            chain_type_kwargs={'prompt': qa_prompt})
-    
-    if not is_philosophy_related.remote(query):
-        return ("This is not my area of expertise.")
     
     response_dict = qa_chain({'query': query})  
 
